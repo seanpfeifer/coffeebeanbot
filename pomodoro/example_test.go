05921ed99d4890d5e173e63cd803464525e8b2aa
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2019-05-25 00:28:41 -0700

    Modified example package
    This is a workaround for a complaint in "Go Report Card", but is otherwise not really a fix.  The prior state was generally preferable in Go, but doesn't make a big difference in this tiny case.

--- pomodoro/example_test.go
+++ pomodoro/example_test.go
@@ -1,26 +1,24 @@
-package pomodoro_test
+package pomodoro
 
 // Copyright 2017 Sean A. Pfeifer
 
 import (
 	"fmt"
 	"time"
-
-	"github.com/seanpfeifer/coffeebeanbot/pomodoro"
 )
 
 func ExampleNewPomodoro() {
 	// This channel will prevent us from exiting the test before our Pomodoro has completed
 	c := make(chan bool)
-	onTestEnd := func(notify pomodoro.NotifyInfo, completed bool) {
+	onTestEnd := func(notify NotifyInfo, completed bool) {
 		if completed {
 			fmt.Printf("Work '%s' done!\n", notify.Title)
 		}
 		c <- true
 	}
 
-	notify := pomodoro.NotifyInfo{Title: "Create example"}
-	pomodoro.NewPomodoro(time.Millisecond*2, onTestEnd, notify)
+	notify := NotifyInfo{Title: "Create example"}
+	NewPomodoro(time.Millisecond*2, onTestEnd, notify)
 
 	<-c
 	fmt.Println("Exiting test.")
