85018fe7fff7fd9ba782df125f7784968c069389
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2022-02-10 01:01:53 -0800

    Converted bot to slash commands
    Moved now-deprecated chat commands to deprecated.go. This makes it very clear that this really is going away soon, and exists just for transition.

--- coffeebeanbot.go
+++ coffeebeanbot.go
@@ -3,7 +3,6 @@
 package coffeebeanbot
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"os"
@@ -18,29 +17,26 @@ import (
 )
 
 const (
-	discordBotPrefix    = "Bot "
-	pomDuration         = time.Minute * 25
-	voiceWaitTime       = time.Millisecond * 250 // The amount of time to sleep before speaking & leaving the voice channel
-	baseAuthURLTemplate = "https://discordapp.com/api/oauth2/authorize?client_id=%s&scope=bot"
+	discordBotPrefix = "Bot "
+	pomDuration      = time.Minute * 25
+	voiceWaitTime    = time.Millisecond * 250 // The amount of time to sleep before speaking & leaving the voice channel
+	startCmdName     = "pomstart"
+	cancelCmdName    = "pomcancel"
+	flagEphemeral    = 1 << 6 // The flag that specifies that a message is "ephemeral". ie, only visible to the caller
 )
 
 // cmdHandler is the type for our functions that will be called upon receiving commands from a user.
 type cmdHandler func(s *discordgo.Session, m *discordgo.MessageCreate, extra string)
 
-type botCommand struct {
-	handler       cmdHandler
-	desc          string
-	exampleParams string
-}
+type appCmdHandler func(*discordgo.Session, *discordgo.Interaction)
 
 // Bot contains the information needed to run the Discord bot
 type Bot struct {
-	Config      Config
-	secrets     Secrets
-	cmdHandlers map[string]botCommand
-	discord     *discordgo.Session
-	logger      Logger
-	metrics     metrics.Recorder
+	Config  Config
+	secrets Secrets
+	discord *discordgo.Session
+	logger  Logger
+	metrics metrics.Recorder
 
 	helpMessage        string
 	poms               pomodoro.ChannelPomMap
@@ -57,8 +53,6 @@ func NewBot(config Config, secrets Secrets, logger Logger, recorder metrics.Reco
 		poms:    pomodoro.NewChannelPomMap(),
 	}
 
-	bot.registerCmdHandlers()
-	bot.helpMessage = bot.buildHelpMessage()
 	bot.loadSounds()
 
 	return bot
@@ -71,28 +65,31 @@ func (bot *Bot) loadSounds() {
 	}
 }
 
-func (bot *Bot) registerCmdHandlers() {
-	bot.cmdHandlers = map[string]botCommand{
-		"start":  {handler: bot.onCmdStartPom, desc: "Starts a Pomodoro work cycle on the channel. You can optionally specify the task you are working on", exampleParams: "Create a new notification sound, add an example"},
-		"cancel": {handler: bot.onCmdCancelPom, desc: "Cancels the current Pomodoro work cycle on the channel", exampleParams: ""},
-		"help":   {handler: bot.onCmdHelp, desc: "Shows this help message", exampleParams: ""},
-	}
-}
+func (bot *Bot) registerAppCmds() error {
+	// Intentionally not using the returned commands - we have no use for them, just the const names that we already have,
+	// which we'll use to determine which command a person has triggered.
+	_, err := bot.discord.ApplicationCommandBulkOverwrite(bot.secrets.AppID, "", []*discordgo.ApplicationCommand{
+		{
+			Name:        startCmdName,
+			Description: "Starts a Pomodoro work cycle on the channel. You can optionally specify the task you are working on.",
+			Type:        discordgo.ChatApplicationCommand,
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:         "task",
+					Type:         discordgo.ApplicationCommandOptionString,
+					Description:  "The task you are working on",
+					ChannelTypes: []discordgo.ChannelType{discordgo.ChannelTypeGuildText},
+				},
+			},
+		},
+		{
+			Name:        cancelCmdName,
+			Description: "Cancels the current Pomodoro work cycle on the channel",
+			Type:        discordgo.ChatApplicationCommand,
+		},
+	})
 
-func (bot *Bot) buildHelpMessage() string {
-	helpBuf := bytes.Buffer{}
-	helpBuf.WriteString("This bot was written by Sean Pfeifer to help him get more done.\n")
-
-	// I don't really care about ordering right now - this is intentionally using the map iteration order,
-	// which I am aware is pseudo-random.
-	for cmdStr, cmd := range bot.cmdHandlers {
-		helpBuf.WriteString(fmt.Sprintf("\n•  **%s**  -  %s\n", cmdStr, cmd.desc))
-		helpBuf.WriteString(fmt.Sprintf("    Example: `%s%s %s`\n", bot.Config.CmdPrefix, cmdStr, cmd.exampleParams))
-	}
-
-	helpBuf.WriteString("\nTo have me join your server, click on my name and 'Add to Server'.")
-
-	return helpBuf.String()
+	return err
 }
 
 // Start will start the bot, blocking until completion
@@ -108,13 +105,20 @@ func (bot *Bot) Start() error {
 	}
 
 	bot.discord.AddHandler(bot.onReady)
-	bot.discord.AddHandler(bot.onMessageReceived)
+	bot.discord.AddHandler(bot.onDeprecatedMessage) // Deprecated - will be removed when this functionality is disabled by Discord
+	// Our app command handler, which dispatches all incoming commands
+	bot.discord.AddHandler(bot.onAppCmd)
 	// Simply for keeping track of how many guilds we're a part of (to monitor bot health)
 	bot.discord.AddHandler(bot.onGuildCreate)
 	bot.discord.AddHandler(bot.onGuildDelete)
 
 	err = bot.discord.Open()
 	if err != nil {
+		return err
+	}
+
+	defer bot.discord.Close()
+	if err := bot.registerAppCmds(); err != nil {
 		return err
 	}
 
@@ -131,59 +135,40 @@ func (bot *Bot) onReady(s *discordgo.Session, event *discordgo.Ready) {
 	bot.metrics.RecordConnectedServers(numGuilds)
 }
 
-// onMessageReceived is called when a message is received on a channel that the bot is listening on.
-// It will dispatch known commands to the command handlers, passing along any extra string information.
-func (bot *Bot) onMessageReceived(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore messages created by this bot
-	if m.Author.ID == s.State.User.ID {
+func (bot *Bot) onAppCmd(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// Ignore anything that's not an app cmd
+	if i.Type != discordgo.InteractionApplicationCommand {
 		return
 	}
+	bot.logger.Info("appCmd", fmt.Sprintf("%+v", *i))
 
-	msg := m.Content
-
-	cmdPrefixLen := len(bot.Config.CmdPrefix)
-
-	// Dispatch the command iff we have our prefix (case-insensitive).
-	if len(msg) > cmdPrefixLen && strings.EqualFold(bot.Config.CmdPrefix, msg[0:cmdPrefixLen]) {
-		afterPrefix := msg[cmdPrefixLen:]
-		cmd := strings.SplitN(afterPrefix, " ", 2)
-
-		if f, ok := bot.cmdHandlers[strings.ToLower(cmd[0])]; ok {
-			rest := ""
-			if len(cmd) > 1 {
-				rest = cmd[1]
-			}
-
-			if f.handler != nil {
-				f.handler(s, m, rest)
-			} else {
-				bot.logger.Error("nil handler for command", "command", cmd)
-				s.ChannelMessageSend(m.ChannelID, "Command error - please contact support.")
-			}
-		}
+	data := i.ApplicationCommandData()
+	switch data.Name {
+	case startCmdName:
+		bot.onAppCmdStart(s, i.Interaction)
+	case cancelCmdName:
+		bot.onAppCmdCancel(s, i.Interaction)
 	}
 }
 
-func (bot *Bot) onCmdHelp(s *discordgo.Session, m *discordgo.MessageCreate, extra string) {
-	s.ChannelMessageSend(m.ChannelID, bot.helpMessage)
-}
+func (bot *Bot) onAppCmdStart(s *discordgo.Session, i *discordgo.Interaction) {
+	data := i.ApplicationCommandData()
+	var task string
+	if len(data.Options) > 0 {
+		task = data.Options[0].StringValue()
+	}
 
-func (bot *Bot) onCmdStartPom(s *discordgo.Session, m *discordgo.MessageCreate, extra string) {
-	channel, err := s.State.Channel(m.ChannelID)
-	if LogIfError(bot.logger, err, "Could not find channel", "channelID", m.ChannelID) {
+	channel, err := s.State.Channel(i.ChannelID)
+	if LogIfError(bot.logger, err, "Could not find channel", "channelID", i.ChannelID) {
 		// Could not find the channel, so simply log and exit
 		return
 	}
 
-	// Make sure the user's text can't break out of our quote box.
-	extra = strings.Replace(extra, "`", "", -1)
-	extra = strings.TrimSpace(extra)
-
 	notif := pomodoro.NotifyInfo{
-		Title:     extra,
-		UserID:    m.Author.ID,
+		Title:     task,
+		UserID:    i.Member.User.ID,
 		GuildID:   channel.GuildID,
-		ChannelID: m.ChannelID,
+		ChannelID: i.ChannelID,
 	}
 
 	if bot.poms.CreateIfEmpty(pomDuration, bot.onPomEnded, notif) {
@@ -193,19 +178,42 @@ func (bot *Bot) onCmdStartPom(s *discordgo.Session, m *discordgo.MessageCreate, 
 		}
 
 		msg := fmt.Sprintf("%s**%.1f minutes** remaining!", taskStr, pomDuration.Minutes())
-		s.ChannelMessageSend(m.ChannelID, msg)
+		s.InteractionRespond(i, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: msg,
+			},
+		})
 		bot.metrics.RecordStartPom()
 		bot.metrics.RecordRunningPoms(int64(bot.poms.Count()))
 	} else {
-		s.ChannelMessageSend(m.ChannelID, "A Pomodoro is already running on this channel.")
+		s.InteractionRespond(i, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "A Pomodoro is already running on this channel.",
+				Flags:   flagEphemeral,
+			},
+		})
 	}
 }
 
-func (bot *Bot) onCmdCancelPom(s *discordgo.Session, m *discordgo.MessageCreate, extra string) {
-	if exists := bot.poms.RemoveIfExists(m.ChannelID); !exists {
-		s.ChannelMessageSend(m.ChannelID, "No Pomodoro running on this channel to cancel.")
+func (bot *Bot) onAppCmdCancel(s *discordgo.Session, i *discordgo.Interaction) {
+	if exists := bot.poms.RemoveIfExists(i.ChannelID); !exists {
+		s.InteractionRespond(i, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "No Pomodoro running on this channel to cancel.",
+				Flags:   flagEphemeral,
+			},
+		})
+	} else {
+		s.InteractionRespond(i, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Pomodoro cancelled!",
+			},
+		})
 	}
-	// If this removal succeeds, then the onPomEnded callback will be called, so we don't need to do anything here.
 }
 
 // onPomEnded performs the notification
@@ -232,9 +240,8 @@ func (bot *Bot) onPomEnded(notif pomodoro.NotifyInfo, completed bool) {
 		}
 
 		bot.discord.ChannelMessageSend(notif.ChannelID, message)
-	} else {
-		bot.discord.ChannelMessageSend(notif.ChannelID, "Pomodoro cancelled!")
 	}
+	// Otherwise this was cancelled, and the reply will already be sent by the app cmd
 
 	bot.metrics.RecordRunningPoms(int64(bot.poms.Count()))
 }
