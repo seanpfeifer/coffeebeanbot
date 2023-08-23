6bb75446524dde30177ab16e6519b3be04b0a97c
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2023-08-22 22:36:22 -0700

    Swap logging to use "log/slog" (#13)

--- log.go
+++ log.go
@@ -1,21 +1,13 @@
 package coffeebeanbot
 
-// Logger is the interface that all logs are expected to use with this package.
-type Logger interface {
-	// Info logs a message with some additional context as key-value pairs
-	Info(msg string, kvPairs ...any)
-	// Error logs a message with some additional context as key-value pairs
-	Error(msg string, kvPairs ...any)
-	// Named adds a sub-scope to the logger's name
-	Named(name string) Logger
-}
+import "log/slog"
 
-// LogIfError will log the [error + extra info] if the error is non-nil.
+// LogIfError will log the [error + args as key:value pairs] if the error is non-nil.
 // Returns true if err is non-nil.
-func LogIfError(logger Logger, err error, msg string, extraInfo ...any) bool {
+func LogIfError(logger *slog.Logger, err error, msg string, args ...any) bool {
 	if err != nil {
-		info := append([]any{"error", err}, extraInfo...)
-		logger.Error(msg, info...)
+		kv := append([]any{"error", err}, args...)
+		logger.Error(msg, kv...)
 		return true
 	}
 	return false
